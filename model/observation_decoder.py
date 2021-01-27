80ad831667c73e3a2f2bd8cb51e41cc836c5ad08
Oliver Ubuntu <3bartels@informatik@uni-hamburg.de>
2021-01-27 16:54:57 +0100

    quick fix for ObservationModelDecoder

--- model/observation_decoder.py
+++ model/observation_decoder.py
@@ -1,4 +1,3 @@
-from torch.nn import functional as F
 import torch
 from torch.nn import Module, ConvTranspose2d, Linear
 from torch import nn
@@ -7,15 +6,13 @@ from torch import nn
 class ObservationModelDecoder(Module):
 
     def __init__(self, state_dim: int, hidden_dim: int, activation_func: str = 'ReLU') -> None:
-        super(ObservationModelDecoder, self).__init__()
+        super().__init__()
         activation_function = getattr(nn, activation_func)
-        self.lin = Linear(state_dim + hidden_dim, 1024)
-        self.net = nn.Sequential(ConvTranspose2d(1024, 128, kernel_size=5, stride=2), activation_function(),
+        self.net = nn.Sequential(Linear(state_dim + hidden_dim, 1024),
+                                 ConvTranspose2d(1024, 128, kernel_size=5, stride=2), activation_function(),
                                  ConvTranspose2d(128, 64, kernel_size=5, stride=2), activation_function(),
                                  ConvTranspose2d(64, 32, kernel_size=6, stride=2), activation_function(),
                                  ConvTranspose2d(32, 3, kernel_size=6, stride=2), activation_function())
 
-    def forward(self, state, hidden_state) -> torch.Tensor:
-        latent = self.lin(torch.cat([state, hidden_state], dim=1))
-        obs = self.net.forward(latent)
-        return obs
+    def forward(self, state: torch.Tensor, hidden_state: torch.Tensor) -> torch.Tensor:
+        return self.net.forward(torch.cat([state, hidden_state], dim=1))
