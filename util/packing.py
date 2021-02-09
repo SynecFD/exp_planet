78549e2a84f509fcb1948ae49640f1e0c14f2b1a
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-02-09 19:55:30 +0100

    Pack padded sequence in Tensor-space

--- util/packing.py
+++ util/packing.py
@@ -16,24 +16,25 @@ def split_into_batch_sequences(tensor: Tensor, seq_lengths: List[int]) -> Tuple[
     return torch.split(tensor, seq_lengths)
 
 
-def pack_padded_sequence(batch: Tensor, lengths: List[int], enforce_sorted: bool = True) -> PackedSequence:
+def pack_padded_sequence(batch: Tensor, lengths: Tensor, enforce_sorted: bool = True) -> PackedSequence:
     if enforce_sorted and not is_sorted(lengths, reverse=True):
-        rows = list(range(batch.size(0)))
-        # https://stackoverflow.com/a/45514542/4907774
-        lengths, rows = map(list, sort_together([lengths, rows], reverse=True))
-        batch = batch[rows, ...]
+        lengths, sorted_indices = torch.sort(lengths, descending=True)
+        sorted_indices = sorted_indices.to(batch.device)
+        batch = batch.index_select(0, sorted_indices)
     return torch.nn.utils.rnn.pack_padded_sequence(batch, lengths, batch_first=True, enforce_sorted=enforce_sorted)
 
 
 def pad_sequence(batch_seq: Union[Tuple[Tensor, ...], List[Tensor]], enforce_sorted: bool = True) \
-        -> Tuple[Tensor, List[int]]:
+        -> Tuple[Tensor, Tensor]:
     lengths = [seq.size(0) for seq in batch_seq]
     if not all_equal(lengths):
         # Sorting due to ``enforce_sorted = True`` default flag, for potential ONNX export.
         if enforce_sorted and not is_sorted(lengths, reverse=True):
             # https://stackoverflow.com/a/45514542/4907774
             lengths, batch_seq = map(list, sort_together([lengths, batch_seq], reverse=True))
-        return torch.nn.utils.rnn.pad_sequence(batch_seq, batch_first=True), lengths
+        padded = torch.nn.utils.rnn.pad_sequence(batch_seq, batch_first=True)
+        return padded, torch.as_tensor(lengths, device=padded.device)
     else:
         # Batch-first
-        return torch.stack(batch_seq), lengths
+        stacked = torch.stack(batch_seq)
+        return stacked, torch.as_tensor(lengths, device=stacked.device)
