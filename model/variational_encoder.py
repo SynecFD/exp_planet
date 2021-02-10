f3ef7c013d8973859533250f52cb0589f4dafcd7
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-02-10 02:07:28 +0100

    VAE: Flat in sequential-container instead of forward-call

--- model/variational_encoder.py
+++ model/variational_encoder.py
@@ -9,9 +9,8 @@ class VariationalEncoder(nn.Module):
         self.net = nn.Sequential(nn.Conv2d(3, 32, kernel_size=4, stride=2), activation_function(),
                                  nn.Conv2d(32, 64, kernel_size=4, stride=2), activation_function(),
                                  nn.Conv2d(64, 128, kernel_size=4, stride=2), activation_function(),
-                                 nn.Conv2d(128, 256, kernel_size=4, stride=2), activation_function())
+                                 nn.Conv2d(128, 256, kernel_size=4, stride=2), activation_function(),
+                                 nn.Flatten())
 
     def forward(self, obs):
-        latent = self.net.forward(obs)
-        latent = latent.view(-1, 1024)
-        return latent
+        return self.net.forward(obs)
