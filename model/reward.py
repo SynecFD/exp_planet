2b2855e1ac5a0bf3fc38ae4793754701a62dc1b5
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-02-23 12:19:08 +0100

    Fix Reward model for batch-seq tensors

--- model/reward.py
+++ model/reward.py
@@ -8,8 +8,12 @@ class RewardModel(nn.Module):
     p(r_t | s_t, h_t)
     """
 
-    def __init__(self, state_dim: int, hidden_input_dim: int, hidden_units_dim: int = 300,
-                 activation_func: str = 'ReLU') -> None:
+    def __init__(self,
+                 state_dim: int,
+                 hidden_input_dim: int,
+                 hidden_units_dim: int = 300,
+                 activation_func: str = 'ReLU'
+                 ) -> None:
         super().__init__()
         activation_function = getattr(nn, activation_func)
         self.net = nn.Sequential(nn.Linear(state_dim + hidden_input_dim, hidden_units_dim), activation_function(),
@@ -18,8 +22,8 @@ class RewardModel(nn.Module):
                                  nn.Linear(hidden_units_dim, 1), activation_function())
 
     def forward(self, state, hidden_state):
-        hidden = self.net.forward(torch.cat([state, hidden_state], dim=1))
-        mean_reward = hidden.squeeze()  # FIXME: maybe only for dim=1
+        hidden = self.net.forward(torch.cat([state, hidden_state], dim=2))
+        mean_reward = hidden.squeeze(dim=2)
         # Note that the log-likelihood under a Gaussian distribution with unit variance equals
         # the mean squared error up to a constant.
         return mean_reward
