2457482ea26e6d6038c865492997c0445b1791d3
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-03-01 00:20:03 +0100

    Make VAE aware of HxW

--- model/observation_decoder.py
+++ model/observation_decoder.py
@@ -4,14 +4,15 @@ from torch import nn
 
 class ObservationModelDecoder(nn.Module):
 
-    def __init__(self, state_dim: int, hidden_dim: int, activation_func: str = 'ReLU') -> None:
+    def __init__(self, state_dim: int, hidden_dim: int, height: int, width: int, activation_func: str = 'ReLU') -> None:
         super().__init__()
         activation_function = getattr(nn, activation_func)
         self.net = nn.Sequential(nn.Linear(state_dim + hidden_dim, 1024),
                                  nn.ConvTranspose2d(1024, 128, kernel_size=5, stride=2), activation_function(),
                                  nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2), activation_function(),
                                  nn.ConvTranspose2d(64, 32, kernel_size=6, stride=2), activation_function(),
-                                 nn.ConvTranspose2d(32, 3, kernel_size=6, stride=2), activation_function())
+                                 nn.ConvTranspose2d(32, 3, kernel_size=6, stride=2), activation_function(),
+                                 nn.Unflatten(1, (3, height, width)))
 
     def forward(self, state: torch.Tensor, hidden_state: torch.Tensor) -> torch.Tensor:
         return self.net.forward(torch.cat([state, hidden_state], dim=1))
--- model/variational_encoder.py
+++ model/variational_encoder.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class VariationalEncoder(nn.Module):
 
-    def __init__(self, activation_function: str = 'ReLU') -> None:
+    def __init__(self, height: int, width: int, activation_function: str = 'ReLU') -> None:
         super().__init__()
         activation_function = getattr(nn, activation_function)
         self.net = nn.Sequential(nn.Conv2d(3, 32, kernel_size=4, stride=2), activation_function(),
@@ -11,6 +11,24 @@ class VariationalEncoder(nn.Module):
                                  nn.Conv2d(64, 128, kernel_size=4, stride=2), activation_function(),
                                  nn.Conv2d(128, 256, kernel_size=4, stride=2), activation_function(),
                                  nn.Flatten())
+        self._latent_size = self.latent_size(height, width)
 
     def forward(self, obs):
         return self.net.forward(obs)
+
+    def _latent_size(self, height: int, width: int) -> int:
+        latent_dim_x = height
+        latent_dim_y = width
+        out_channels = 3
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                latent_dim_x = (latent_dim_x + 2 * module.padding[0] - module.dilation[0] * (
+                            module.kernel_size[0] - 1) - 1) // module.stride[0] + 1
+                latent_dim_y = (latent_dim_y + 2 * module.padding[1] - module.dilation[1] * (
+                            module.kernel_size[1] - 1) - 1) // module.stride[1] + 1
+                out_channels = module.out_channels
+        return latent_dim_x * latent_dim_y * out_channels
+
+    @property
+    def laten_size(self):
+        return self._latent_size
