3123aef599c76720bbd9cc705087add40dd09c07
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-03-01 18:29:42 +0100

    Agent: can now be reset properly

--- agent/env_agent.py
+++ agent/env_agent.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional, Union
 
 import gym
+import numpy as np
 import torch
 from torch.distributions import Normal
 
@@ -33,9 +35,10 @@ class Agent:
         self.env = env
 
         self.state = None
-        self.action_dim = self.action_space.shape.sum()
+        self.action_dim = sum(self.action_space.shape)
 
     def reset(self) -> torch.Tensor:
+        self.replay_buffer.stack_episode()
         self.env.reset()
         self.state = self.env.render(mode="rgb_array")
         return self.state
@@ -45,13 +48,17 @@ class Agent:
         action_mean = self.planner(obs)
         return Normal(action_mean, self.explore_noise).sample()
 
-    def step(self):
-        action = self.action(self.state).numpy()
+    def step(self, action: Optional[Union[np.ndarray, torch.Tensor]] = None) -> tuple[int, bool]:
+        if action is None:
+            action = self.action(self.state).numpy()
+        elif isinstance(action, torch.Tensor):
+            action = action.numpy()
         _, reward, done, _ = self.env.step(action)
         self.state = self.env.render(mode="rgb_array")
         self.replay_buffer.add_step_data(self.state, action, reward)
         if done:
-            self.replay_buffer.stack_episode()
+            self.reset()
+        return reward, done
 
     @property
     def action_space(self):
