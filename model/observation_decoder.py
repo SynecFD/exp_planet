cf8b5965ec4776d6c675ea55238d51c1af263e2b
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-03-04 02:24:52 +0100

    Auto-reshape batch-sequences for conv-layers

--- model/observation_decoder.py
+++ model/observation_decoder.py
@@ -7,12 +7,14 @@ class ObservationModelDecoder(nn.Module):
     def __init__(self, state_dim: int, hidden_dim: int, height: int, width: int, activation_func: str = 'ReLU') -> None:
         super().__init__()
         activation_function = getattr(nn, activation_func)
-        self.net = nn.Sequential(nn.Linear(state_dim + hidden_dim, 1024),
+        self.net = nn.Sequential(nn.Flatten(0, 1),
+                                 nn.Linear(state_dim + hidden_dim, 1024), nn.Unflatten(1, (1024, 1, 1)),
                                  nn.ConvTranspose2d(1024, 128, kernel_size=5, stride=2), activation_function(),
                                  nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2), activation_function(),
                                  nn.ConvTranspose2d(64, 32, kernel_size=6, stride=2), activation_function(),
-                                 nn.ConvTranspose2d(32, 3, kernel_size=6, stride=2), activation_function(),
-                                 nn.Unflatten(1, (3, height, width)))
+                                 nn.ConvTranspose2d(32, 3, kernel_size=6, stride=2), activation_function())
 
     def forward(self, state: torch.Tensor, hidden_state: torch.Tensor) -> torch.Tensor:
-        return self.net.forward(torch.cat([state, hidden_state], dim=1))
+        batch_seq_size = state.shape[0:2]
+        recon_obs = self.net.forward(torch.cat([state, hidden_state], dim=2))
+        return recon_obs.unflatten(0, batch_seq_size)
--- model/variational_encoder.py
+++ model/variational_encoder.py
@@ -6,7 +6,8 @@ class VariationalEncoder(nn.Module):
     def __init__(self, height: int, width: int, activation_function: str = 'ReLU') -> None:
         super().__init__()
         activation_function = getattr(nn, activation_function)
-        self.net = nn.Sequential(nn.Conv2d(3, 32, kernel_size=4, stride=2), activation_function(),
+        self.net = nn.Sequential(nn.Flatten(0, 1),
+                                 nn.Conv2d(3, 32, kernel_size=4, stride=2), activation_function(),
                                  nn.Conv2d(32, 64, kernel_size=4, stride=2), activation_function(),
                                  nn.Conv2d(64, 128, kernel_size=4, stride=2), activation_function(),
                                  nn.Conv2d(128, 256, kernel_size=4, stride=2), activation_function(),
@@ -14,7 +15,9 @@ class VariationalEncoder(nn.Module):
         self._latent_size = self._latent_size(height, width)
 
     def forward(self, obs):
-        return self.net.forward(obs)
+        batch_seq_size = obs.shape[0:2]
+        latent = self.net.forward(obs)
+        return latent.unflatten(0, batch_seq_size)
 
     def _latent_size(self, height: int, width: int) -> int:
         latent_dim_x = height
@@ -23,9 +26,9 @@ class VariationalEncoder(nn.Module):
         for module in self.modules():
             if isinstance(module, nn.Conv2d):
                 latent_dim_x = (latent_dim_x + 2 * module.padding[0] - module.dilation[0] * (
-                            module.kernel_size[0] - 1) - 1) // module.stride[0] + 1
+                        module.kernel_size[0] - 1) - 1) // module.stride[0] + 1
                 latent_dim_y = (latent_dim_y + 2 * module.padding[1] - module.dilation[1] * (
-                            module.kernel_size[1] - 1) - 1) // module.stride[1] + 1
+                        module.kernel_size[1] - 1) - 1) // module.stride[1] + 1
                 out_channels = module.out_channels
         return latent_dim_x * latent_dim_y * out_channels
 
