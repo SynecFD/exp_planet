e54419497e7c524c04247bb3fcda0a523b5b27b9
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-03-04 13:03:57 +0100

    Mask the loss according to padding

--- model/loss.py
+++ model/loss.py
@@ -17,8 +17,8 @@ def loss(prior: Normal,
     total_seq_length = obs.size(1)
     mask = torch.arange(total_seq_length)[None, :] < lengths[:, None]
 
-    kl_loss = kl_divergence(prior, posterior).masked_select(mask).sum(dim=2).clamp(min=free_nats).mean(dim=(0, 1))
-    obs_loss = F.mse_loss(decode_obs, obs, reduction="none").sum(dim=(2, 3, 4)).mean(dim=(0, 1))
-    reward_loss = F.mse_loss(expected_reward, reward)
+    kl_loss = kl_divergence(prior, posterior).sum(dim=2).masked_select(mask).clamp(min=free_nats).mean()
+    obs_loss = F.mse_loss(decode_obs, obs, reduction="none").sum(dim=(2, 3, 4)).masked_select(mask).mean()
+    reward_loss = F.mse_loss(expected_reward, reward, reduction="none").masked_select(mask).mean()
 
     return kl_loss + obs_loss + reward_loss
