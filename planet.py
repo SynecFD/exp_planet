820ae9fcdf6f9f76e30ced91be63dab5ad75c744
Niklas H <Typhoon.CommanderCool@gmail.com>
2021-03-05 01:05:06 +0100

    Refactor loss into lightning module

--- planet.py
+++ planet.py
@@ -10,7 +10,8 @@ import gym
 import pybullet_envs
 import pytorch_lightning as pl
 import torch
-from torch.distributions import Normal
+import torch.nn.functional as F
+from torch.distributions import Normal, kl_divergence
 from torch.nn import Parameter
 from torch.utils.data import DataLoader
 
@@ -152,6 +153,26 @@ class PlaNet(pl.LightningModule):
         for _ in range(self.episode_max_length):
             self.agent.step()
         self.agent.reset()
+
+    def single_step_loss(self, prior: Normal,
+                         posterior: Normal,
+                         obs: torch.Tensor,
+                         decode_obs: torch.Tensor,
+                         expected_reward: torch.Tensor,
+                         reward: torch.Tensor,
+                         lengths: torch.Tensor,
+                         free_nats: int = 3
+                         ) -> torch.Tensor:
+        # https://discuss.pytorch.org/t/how-to-generate-variable-length-mask/23397
+        # https://juditacs.github.io/2018/12/27/masked-attention.html
+        total_seq_length = obs.size(1)
+        mask = torch.arange(total_seq_length, device=self.device)[None, :] < lengths[:, None]
+
+        kl_loss = kl_divergence(prior, posterior).sum(dim=2).masked_select(mask).clamp(min=free_nats).mean()
+        obs_loss = F.mse_loss(decode_obs, obs, reduction="none").sum(dim=(2, 3, 4)).masked_select(mask).mean()
+        reward_loss = F.mse_loss(expected_reward, reward, reduction="none").masked_select(mask).mean()
+
+        return kl_loss + obs_loss + reward_loss
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
         return torch.optim.Adam(self._get_params(), lr=self.lr, eps=self.epsi)
